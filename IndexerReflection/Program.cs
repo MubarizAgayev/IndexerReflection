5052e14f33214399670755fe169169fc46206b49
mubarizra@code.edu.az <mubarizra@code.edu.az>
2023-01-10 22:22:22 +0400

    IndexerReflection

---
+++ IndexerReflection/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+using IndexerReflection;
+using IndexerReflection.Controllers;
+using System.Reflection;
+
+//Book book1 = new Book();
+//book1.Name = "Xosrov";
+//Book book2 = new Book();
+//book2.Name = "7 gozel";
+
+//Library library = new Library();
+//library[0] = book1;
+//library[1] = book2;
+//Console.WriteLine(library[0].Name);
+//Console.WriteLine(library[1].Name);
+
+
+
+//Assembly assembly = Assembly.GetExecutingAssembly();
+//foreach (var item in assembly.GetTypes())
+//{
+//	foreach (var item2 in item.GetMembers())
+//	{
+//		Console.WriteLine(item2);
+//	}
+//}
+
+
+
+//AccountController account=new AccountController();
+//account.Register();
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ServiceLayer/Services/AccountService.cs
@@ -0,0 +1,41 @@
+﻿using ServiceLayer.Exceptions;
+using ServiceLayer.Helpers.Constants;
+using ServiceLayer.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Services
+{
+    public class AccountService:IAccountService
+    {
+        public string Register(string username, string email, string password)
+        {
+            try
+            {
+                var res = GetAllEmails();
+                if (res.Contains(email))
+                {
+                    throw new RegisterExceptions(ResponseMessages.InvalidRegisterMessage);
+                }
+                return ResponseMessages.SuccessRegisterMessage;
+            }
+
+            catch (Exception ex)
+            {
+
+                return ex.Message;
+            }
+
+
+        }
+
+        private List<string> GetAllEmails()
+        {
+            return new List<string>() { "mirze@code.edu.az", "cahandar@code.edu.az", "cavid@code.edu.az", "konul@code.edu.az" };
+
+        }
+    }
+}
